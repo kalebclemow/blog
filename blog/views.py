e66d0e38ab35c66a151d4618c7b487880b3717f6
Kaleb Clemow <kc181349@umconnect.umt.edu>
2022-10-04 11:50:53 -0600

    ch 6 update

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
@@ -13,3 +14,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
