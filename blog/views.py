8a92b588f5b027732555433270d0253b16aed2e2
Kaleb Clemow <kc181349@umconnect.umt.edu>
2022-10-10 19:50:05 -0600

    chapter seven update

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,10 @@
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
 from .models import Post
 
 
@@ -19,3 +22,14 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
